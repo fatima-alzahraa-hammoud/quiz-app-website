36cf2dfb6c02bf75f32e8947529f084c079228d6
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-12-05 02:35:23 +0200

    React <> Add - Quizzes Component

---
+++ frontend/src/components/quiz-components/Quizzes.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+//import { requestApi } from "../../utils/request";
+import "../../styles/base/utilities.css";
+//import "../../styles/quizzes.css";
+import QuizCard from "./QuizCard";
+
+const Quizzes = () => {
+
+    const [quizz, setQuizzes] = useState([]);
+    const quizzes = [
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1},
+        {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1}
+    ]
+
+    return (
+        <div className="quizzes-container">
+            <div className="quizzes-section">
+                <h2 className="section-title">Quizzes</h2>
+                <div className="flex center wrap quizzes-grid">
+                    {quizzes.map((quiz) => (
+                        <QuizCard
+                            quiz={quiz}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Quizzes;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "../components/common/HeroSection";
 import Navbar from "../components/common/Navbar";
 import QuizCard from "../components/quiz-components/QuizCard";
+import Quizzes from "../components/quiz-components/Quizzes";
 
 const Home = () => {
   const quiz = {topic: "Hello", type: "C++", duration:"1h", nbOfQuestions: 4, solvedBy: 34, nbOfAttempts: 2, quizId: 1};
@@ -10,7 +11,7 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
-      <QuizCard quiz={quiz}/>
+      <Quizzes />
     </div>
   );
 
